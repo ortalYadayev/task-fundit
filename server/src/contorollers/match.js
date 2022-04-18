9dcb60566bb975edf752dbf37fc58d27ae0b8d40
Ortal Yadayev <ortalll20111@gmail.com>
2022-04-18 12:47:09 +0300

    Part 2b - search filters

--- server/src/contorollers/match.js
+++ server/src/contorollers/match.js
@@ -6,14 +6,18 @@ const matchRouter = express.Router();
 matchRouter.get("/api/match", (request, response) => {
     const PAGE_SIZE = 5;
 
-    const page = request.query.page || 1;
+    const page = request.query.page || 0;
+
+    console.log(request.query.page)
+
+    console.log(page)
 
     const paginatedData = tempData.slice(
-        (Number(page) - 1) * PAGE_SIZE,
-        Number(page) * PAGE_SIZE
+        Number(page) * PAGE_SIZE,
+        (Number(page) + 1) * PAGE_SIZE
     );
 
-    response.send(paginatedData);
+    response.send({ data: paginatedData, length: tempData.length });
 });
 
 export default matchRouter;
